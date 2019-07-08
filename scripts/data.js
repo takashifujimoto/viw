5a0a76e085257e25ec79b76de9082b915f22fe1d
Takashi Fujimoto <Takashi.Fujimoto@ssmhealth.com>
2019-07-08 15:59:50 -0500

    initial commit

---
+++ scripts/data.js
@@ -0,0 +1,82 @@
+function load_opening_text(){
+
+    return [
+        '&nbsp;', 
+        'Test', 
+        '&nbsp;T e s t', 
+    ];
+}
+
+function load_opening_text3(){
+
+    return [
+        '&#160;', 
+        '&#160;', 
+        '&#160;', 
+        '&#160;', 
+        '&#160;', 
+        '&#160;', 
+        '&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;Web VIW - Web VI Weakened',
+        '&nbsp;',
+        '&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;version 0.01',
+        '&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160;&#160;Takashi Fujimoto',
+        '&#160;',
+        '&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;Web Viw is open source and freely contributable',
+        '&nbsp',
+        '&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;Help motivated developpers in Dev Community!',
+        '&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;type&#160&#160;&#160;:help contribute&lt;Enter&gt;&#160;&#160;&#160;&#160;for information',
+        '&nbsp;',
+        '&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160;type&#160&#160;&#160;:q&lt;Enter&gt;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;to quit',
+        '&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160;type&#160&#160;&#160;:help&lt;Enter&gt;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;for help'
+    ];
+}
+
+function load_opening_text2(){
+
+    return [
+        '&#160;', 
+        '&#160;', 
+        '&#160;', 
+        '&#160;', 
+        '&#160;', 
+        '&#160;', 
+        '&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;Web VIW - Web VI Weakened',
+        '&nbsp;',
+        '&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;version 0.01',
+        '&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160;&#160;Takashi Fujimoto',
+        '&#160;',
+        '&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;Web Viw is open source and freely contributable',
+        '&nbsp',
+        '&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;Help motivated developpers in Dev Community!',
+        '&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;type&#160&#160;&#160;:help contribute&lt;Enter&gt;&#160;&#160;&#160;&#160;for information',
+        '&nbsp;',
+        '&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160;type&#160&#160;&#160;:q&lt;Enter&gt;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160&#160&#160;&#160;to quit',
+        '&#160;&#160;&#160;&#160&#160&#160;&#160;&#160;&#160;&#160;&#160;type&#160&#160;&#160;:help&lt;Enter&gt;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;for help'
+    ];
+}
+
+
+function load_file(){
+    return [
+        '&nbsp;&nbsp;&nbsp;&nbsp;TEXT',
+        'Name: Lorem Ipsum',
+        '&nbsp',
+        '** PROFILE',
+        'Languages: Dolor, Adipiscing',
+        'Food:  Nostrud, Ullamco, Nisi',
+        'Tools: Wevim, Vim',
+        '&nbsp;',
+        '** PUBLICATIONS',
+        'Nulla Pariatur - Mollit anim id est',
+        'Tempor Incididunt ut Labore et.',
+        '&nbsp;',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        '&nbsp;',
+        'to do',
+        'load text',
+        'move cursor left and right',
+        'move cursor word and back',
+        'move cursor up and down',
+        'Insert Mode',
+    ];
+}
---
+++ scripts/editor.js
@@ -0,0 +1,337 @@
+'use strict';
+
+
+//code from https://www.youtube.com/watch?v=5-koI06rmcA
+var log = console.log;
+
+document.body.style.overflow = 'hidden';
+var editor = document.getElementById('editor');
+
+
+var Mode = {
+    Normal: 0,
+    Insert: 1,
+    Visual: 2,
+    Command:3,
+    Replace: 4,
+};
+
+var vim = {};
+
+(function init(){
+    vim.mode = Mode.Normal;
+    vim.texts = load_opening_text();
+    vim.cursor = {
+        ui: document.getElementById('cursor'),
+        x: 0,
+        y: 0,
+    };
+
+    make_buffer(vim.texts);
+
+    document.body.addEventListener('keydown', body_onKeyDown);   // 1
+
+})( );
+
+
+
+
+
+//function body_onPress(event){
+//    log('press', event);
+//    updateUI( 'status_mode', '<span>' + mode_ToString(vim.mode) + '</span>' );
+//    foo(event);
+//}
+
+function body_onKeyDown(event){
+
+    set_mode(event.code); 
+    updateUI( 'status_mode', '<span>' + mode_ToString(vim.mode) + '</span>' );
+    foo(event);
+}
+
+function foo(event){
+    var key = get_key_downed(event);
+
+    if(vim.mode == Mode.Normal)
+        normal_op(key);
+    else if (vim.mode == Mode.Insert)
+        insert_op(key);
+    else if (vim.mode == Mode.Visual)
+        select_text(key);
+    else if (vim.mode == Mode.Command)
+        send_command(key);
+}
+
+//function select_text(ch){}
+//function send_command(ch){}
+
+function get_key_downed(event){
+    var key = event.key || event.code || event.which;
+    return key;
+    
+}
+
+
+function insert_op(char){
+    if(!vim.newly_insert_mode)
+        insert_charCode(char);
+    vim.newly_insert_mode = false;
+}
+
+
+function insert_charCode(char){
+    var x = vim.cursor.x;
+    var y = vim.cursor.y;
+
+    var  this_line = vim.texts[y];
+    this_line = this_line.substring(0, x+1) + char + this_line.substring(x+1 , this_line.length);
+
+    vim.texts[y] = this_line;
+    var line = 'text_' + y;
+    var elm = document.getElementById(line);
+    elm.innerHTML = make_ui_text_with_cursor_insert_mode(this_line); 
+
+}
+
+
+
+
+function set_mode(code){
+    if(code == 'Escape'){               
+        vim.mode = Mode.Normal;
+    }
+
+    if (code == 'KeyI' || code == 'KeyA') {
+        if(vim.mode !== Mode.Insert){
+            vim.newly_insert_mode = true;
+            vim.mode = Mode.Insert;
+        }
+    }
+
+    if(code == KeyCode.COLON && vim.mode == Mode.Normal){
+        vim.mode = Mode.Command;
+    }
+    
+    if(code == 'keyV'){
+        vim.mode = Mode.Visual;
+    }
+}
+
+
+var KeyCode = {
+    ESC: 27,
+    i:73,
+    v:118,
+    V:86,
+    COLON: 58,
+};
+
+
+
+
+function mode_ToString(mode){
+    var str = 'Normal';
+    switch(mode){
+    case Mode.Normal: 
+        str = 'NORMAL';
+        break;
+    case Mode.Insert: 
+        str = '-- INSERT --';
+        break;
+    case Mode.Visual:
+        str = '-- VISUAL --';
+        break;
+    case Mode.Command:
+        str = ':';
+        break;
+    case Mode.Replace:
+        str = 'Replace';
+        break;
+    }
+    return str;
+}
+
+
+function updateUI( id , content){
+    var element = document.getElementById(id);
+    element.innerHTML = content;
+}
+
+//function run_command(ch){
+function normal_op(ch){
+    switch(ch){
+    case 'k' :
+        if(vim.cursor.y > 0){
+            var y = vim.cursor.y;
+            move_cursor(y, y-1);
+            vim.cursor.y--;
+        } 
+        break;
+
+    case 'j':
+        if(vim.cursor.y < vim.texts.length){
+            move_cursor(vim.cursor.y, vim.cursor.y+1);
+            vim.cursor.y++;
+        } 
+        break;
+
+    case 'l' :
+        if(vim.cursor.x < 100-1){
+            if(vim.texts[vim.cursor.x] == '&'){
+                log('t');
+                move_cursor_x(vim.cursor.x, vim.cursor.y);
+                vim.curosr.x += 6;
+            } else {
+                log('f');
+                move_cursor_x(vim.cursor.x, vim.cursor.y);
+                vim.cursor.x++;
+            }
+        }
+
+        //update this.
+        //editor.innerHTML = '';
+        //make_buffer(vim.texts);
+        break;
+  
+    case 'h':
+        if(vim.cursor.x>0){
+            vim.cursor.x--;
+        }
+        editor.innerHTML = '';
+        make_buffer(vim.texts);
+        break;
+
+    case 'w':
+        if(vim.cursor.x < vim.texts.length-1){
+            while(vim.texts[vim.cursor.x] != ' '){
+                vim.cursor.x++;
+            }
+            vim.cursor.x++;
+        }
+        break;
+    case 'b':
+        while(vim.cursor.x > 0 && vim.texts[vim.cursor.x] != ' ' ){
+            if ( vim.cursor.x != 0)
+                vim.cursor.x--;
+                //else
+            break;               
+        }
+        vim.cursor.x--;
+        while(vim.cursor.x > 0 && vim.texts[vim.cursor.x] != ' ' ){
+            if( vim.cursor.x != 0)
+                vim.cursor.x--;
+            else
+                break;
+        }
+        if( vim.cursor.x != 0)
+            vim.cursor.x++;
+        if( vim.cursor.x < 0){
+            vim.cursor.x = 0;
+        }
+        break;
+    case 'y':
+        if(vim.cursor.x > 0){
+            while(vim.texts[vim.cursor.x] != ' ' || vim.texts[vim.cursor.x] > 0){
+                vim.cursor.x--;
+            }
+            vim.cursor.x--;
+        }
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
+function move_cursor_x(x, y){
+    log('x', x, ' y ', y); 
+    var elm ='line_' + y; 
+    var current_ui_line = document.getElementById(elm);
+    current_ui_line.innerHTML = make_ui_line(vim.texts[y], y);
+
+    //current_ui_line_with_cursor.innerHTML = make_ui_line_with_cursor(vim.texts[y], y);
+}
+
+
+function move_cursor(y, new_y){
+   
+    var elm ='line_' + y; 
+    var current_ui_line = document.getElementById(elm);
+    current_ui_line.innerHTML = make_ui_line(vim.texts[y], y);
+
+    var c_elm = 'line_' + new_y;
+    var next_ui_line_with_cursor = document.getElementById(c_elm);
+    next_ui_line_with_cursor.innerHTML = make_ui_line_with_cursor(vim.texts[new_y], new_y);
+
+}
+
+function make_buffer(texts){
+    for(var i = texts.length; i != -1  ; i--){
+
+
+
+        if ( i == vim.cursor.y) {
+            if(vim.mode == Mode.Normal){
+                editor.innerHTML
+                =  make_ui_line_with_cursor(texts[i], i) + editor.innerHTML;
+            }
+        } else if(vim.mode  == Mode.Normal){
+            editor.innerHTML
+            =  make_ui_line(vim.texts[i], i) + editor.innerHTML;
+        }
+    } 
+    add_unused_line();
+}
+
+
+function make_ui_line(ui_text, i){
+    return     '<div class="line" id="line_'+i+'" >'+
+        '<div class="number" id="nu_'+i+'" >' + i +	'</div>'+
+        '<div class="text" id="text_'+i+'">' + vim.texts[i] +
+        '</div>'+
+        '</div>';
+}
+
+function make_ui_line_with_cursor(ui_text, i){
+   
+    var len = ui_text.length;
+    var x = vim.cursor.x;
+    if (x > len) x = len; 
+
+
+    var regex = /&<\/span>nbsp;/;
+    if(ui_text){
+        return  ('<div class="line" id="line_' + i + '" >'+
+       '<div class="number" id="nu_'+i+'" >' + i +	'</div>'+
+        '<div class="text" id="text_'+i+'" >' +
+         ui_text.substring(0, x) +
+        '<span id="cursor">' + ui_text.substring(x, x+1) +
+        '</span>' + ui_text.substring(x+1, ui_text.length) +
+        '</div>'+
+        '</div>').replace(regex, '&nbsp;</span>');
+    }
+}
+
+
+
+function make_ui_text_with_cursor_insert_mode(ui_text){
+    if(ui_text){
+        vim.cursor.x++;
+
+        return  ui_text.substring(0, vim.cursor.x) +
+        '<span id="cursor_insert">' + ui_text.substring(vim.cursor.x, vim.cursor.x+1) +
+        '</span>' + ui_text.substring(vim.cursor.x+1, ui_text.length) +
+        '</div>';
+    }
+}
+function add_unused_line(){
+    while( window.innerHeight > editor.clientHeight + 249){
+        editor.innerHTML += '<div class="number" ></div><div class="text">~</div>';
+    }
+}
